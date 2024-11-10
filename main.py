54a875b1f8dbe5c97959572f868f3551e9512d36
eck1el <eck1el0222@gmail.com>
2024-11-09 22:55:35 -0600

    añadimos mas pruebas de modelos de entrenamiento como Logistic Regression, Random Forest, Decision Tree y Gaussian Naive Bayes

--- main.py
+++ main.py
@@ -3,8 +3,20 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 import numpy as np
 
-#Este es un modelo clasico de clasificacion
+#Este es un modelo clasico de clasificacion Logistic Regression
 from sklearn.linear_model import LogisticRegression
+
+#Este es el modelo de entrenamiento Random Forest
+from sklearn.ensemble import RandomForestClassifier
+
+#Este es el modelo de entrenamiento DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+#Este es el modelo de entrenamiento Gaussian Naive Bayes
+from sklearn.naive_bayes import GaussianNB
+
+#Este es el modelo de entrenamiento K-Nearest Neighbors
+from sklearn.neighbors import KNeighborsClassifier
 
 from sklearn.model_selection import train_test_split, KFold, cross_val_score
 
@@ -61,13 +73,14 @@ def separarInformacion(titanic_cleaned):
     #El target(lo que queremos predecir) siempre colocarlo en el eje Y
     y = titanic_cleaned["Survived"]
     print(f"Dataset titanic cargado. Numero de registros: {x.shape[0]}")
-
+    print("-----------------------------------")
     dividimosPorcentajes_training_test(x, y)
 
 def dividimosPorcentajes_training_test(x, y):
     #Dividimos en un 20% para test y 80%para training
     X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=20)
     print(f"Hay {X_train.shape[0]} registros en el set de training y {X_test.shape[0]} registros en el set de test")
+    print("-----------------------------------")
     definimosModeloYEntrenamosModelo(X_train, X_test, y_train, y_test)
 
 def definimosModeloYEntrenamosModelo(X_train, X_test, y_train, y_test): #probamos el modelo Logistic Rregression
@@ -84,14 +97,17 @@ def definimosModeloYEntrenamosModelo(X_train, X_test, y_train, y_test): #probamo
     #Si el accuracy se encuentra por encima de 0.85 o debajo de 0.65 significa que no ha entrenado bien y no sirve(indicador de overfitting o sobreajuste)
     print(f"Train Accuracy: {train_score}")
     print(f"Test Accuracy: {test_score}")
-
-    aplicamos_modelo_entrenamiento_cross_validation(X_train, y_train)
+    print("-----------------------------------")
+    entrenamiento_cross_validation_Logistic_regression(X_train, y_train)
+    entrenamiento_cross_validation_Random_forest(X_train, y_train)
+    entrenamiento_cross_validation_Decision_Tree(X_train, y_train)
+    entrenamiento_cross_validation_Gaussian_Naive_Bayes(X_train, y_train)
 
 #este es un modelo de entrenamiento que genera test dentro del porcentaje de datos definido para el entrenamiento
 
-def aplicamos_modelo_entrenamiento_cross_validation(X_train, y_train): #probamos el modelo Logistic Rregression
+def entrenamiento_cross_validation_Logistic_regression(X_train, y_train): #probamos el modelo  de entrenamiento Logistic Rregression
     model = LogisticRegression(max_iter=1000)
-    random_seed = 1 #Semilla para los aleatorios
+    random_seed = 42 #Semilla para los aleatorios
     splits = 5 #Numero de cortes, segmentos o splits para KFolds
     kf = KFold(n_splits=splits, random_state=random_seed, shuffle=True)
 
@@ -108,7 +124,50 @@ def aplicamos_modelo_entrenamiento_cross_validation(X_train, y_train): #probamos
     model.fit(X_train, y_train)
 
     #Comprobamos las metricas tras el entrenamiento
-    print(f"Train Accuracy: {model.score(X_train, y_train)}")
+    print(f"Train Accuracy Logistic Regression: {model.score(X_train, y_train)}")
+    print("-----------------------------------")
+
+def entrenamiento_cross_validation_Random_forest(X_train, y_train): #probamos el modelo de entrenamiento random forest
+    model = RandomForestClassifier()
+    kf =  KFold(n_splits=5, random_state=42, shuffle=True)
+    scores = cross_val_score(model, X_train, y_train, cv=kf, scoring='roc_auc')
+    scores_mean_rf = scores.mean()
+    print(f"Promedio AUC score - Random Forest: {scores_mean_rf}")
+
+    #Entrenamos el modelo
+    model.fit(X_train, y_train)
+
+    #Comprobamos las metricas tras el entrenamiento
+    print(f"Train Accuracy Random Forest: {model.score(X_train, y_train)}")
+    print("-----------------------------------")
+
+def entrenamiento_cross_validation_Decision_Tree(X_train, y_train): #probamos el modelo de entrenamiento decision tree
+    model = DecisionTreeClassifier()
+    kf =  KFold(n_splits=5, random_state=42, shuffle=True)
+    scores = cross_val_score(model, X_train, y_train, cv=kf, scoring='roc_auc')
+    scores_mean_rf = scores.mean()
+    print(f"Promedio AUC score - Decision Tree: {scores_mean_rf}")
+
+    #Entrenamos el modelo
+    model.fit(X_train, y_train)
+
+    #Comprobamos las metricas tras el entrenamiento
+    print(f"Train Accuracy Decision Tree: {model.score(X_train, y_train)}")
+    print("-----------------------------------")
+
+def entrenamiento_cross_validation_Gaussian_Naive_Bayes(X_train, y_train): #probamos el modelo de entrenamiento Gaussian Naive Bayes
+    model = GaussianNB()
+    kf =  KFold(n_splits=5, random_state=42, shuffle=True)
+    scores = cross_val_score(model, X_train, y_train, cv=kf, scoring='roc_auc')
+    scores_mean_rf = scores.mean()
+    print(f"Promedio AUC score - Gaussian Naive Bayes: {scores_mean_rf}")
+
+    #Entrenamos el modelo
+    model.fit(X_train, y_train)
+
+    #Comprobamos las metricas tras el entrenamiento
+    print(f"Train Accuracy Gaussian Naive Bayes: {model.score(X_train, y_train)}")
+    print("-----------------------------------")
 
 def machine_learning():
     data = limpieza_datos()
